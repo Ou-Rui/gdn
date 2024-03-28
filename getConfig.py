8469fcccb5d7283060cafafcd46ee328525426fd
ourui <1447549515@qq.com>
2024-03-28 14:18:43 +0500

    format

--- getConfig.py
+++ getConfig.py
@@ -1,8 +1,7 @@
 def modelArch(in_feature, out_feature):
+  config = [
+    ('linear', [512, in_feature]),
+    ('linear', [out_feature, 512])
+  ]
 
-    config = [
-        ('linear', [512, in_feature]),
-        ('linear', [out_feature, 512])
-    ]
-
-    return config
+  return config
--- models.py
+++ models.py
@@ -5,13 +5,12 @@ import torch.nn.functional as F
 
 
 class SGC(nn.Module):
+  def __init__(self, in_feature, out_feature):
+    super(SGC, self).__init__()
+    self.fc1 = nn.Linear(in_feature, 512)
+    self.out = nn.Linear(512, out_feature)
 
-    def __init__(self, in_feature, out_feature):
-        super(SGC, self).__init__()
-        self.fc1 = nn.Linear(in_feature, 512)
-        self.out = nn.Linear(512, out_feature)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.out(x)
-        return x
+  def forward(self, x):
+    x = self.fc1(x)
+    x = self.out(x)
+    return x
